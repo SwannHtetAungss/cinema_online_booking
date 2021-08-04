2f842222a86cf70d4db4850bf691a48579d07cce
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-08-04 10:30:37 +0630

    second commit

--- database/migrations/2021_08_03_105801_create_movies_table.php
+++ database/migrations/2021_08_03_105801_create_movies_table.php
@@ -16,6 +16,7 @@ class CreateMoviesTable extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('photo');
             $table->string('actor');
             $table->string('actress');
             $table->string('director');
