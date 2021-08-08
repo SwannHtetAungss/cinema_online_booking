4dc03f5107f85a5a97759c337b95a988e38e1a7a
HtutKhaungKyaw17 <htutkyaw506@gmail.com>
2021-08-08 09:37:19 +0630

    allmovie

--- resources/views/frontend/movie.blade.php
+++ resources/views/frontend/movie.blade.php
@@ -5,190 +5,55 @@
 
 @section('content')
 
-        <section class="after-head d-flex section-text-white position-relative">
-            <div class="d-background" data-image-src="http://via.placeholder.com/1920x1080" data-parallax="scroll"></div>
-            <div class="d-background bg-black-80"></div>
-            <div class="top-block top-inner container">
-                <div class="top-block-content">
-                    <h1 class="section-title">Gallery</h1>
-                    <div class="page-breadcrumbs">
-                        <a class="content-link" href="#">Movies</a>
-                        <span class="text-theme mx-2"><i class="fas fa-chevron-right"></i></span>
-                        <span>Gallery</span>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="section-long">
-            <div class="container">
+   
+
+<section class="section-text-white position-relative">
+    <div class="d-background" data-image-src="{{asset('frontend_assets/images/cinema1.jpg')}}" data-parallax="scroll"></div>
+    <div class="d-background bg-theme-blacked"></div>
+    <div class="mt-auto container position-relative">
+        <div class="top-block-head text-uppercase">
+            <h2 class="display-4">Movies
+                <span class="text-theme">Gallery</span>
+            </h2>
+        </div>
+        
+    
+    <div class="container">
                 <div class="grid row">
-                    <div class="col-sm-6 col-lg-4">
+                    @foreach ($movies as $movie)
+                     <div class="col-sm-6 col-lg-4">
                         <div class="gallery-entity">
                             <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
+                                <div class="embed-responsive embed-responsive-poster">
+                                    <img class="embed-responsive-item" src="{{asset('storage/'.$movie->photo)}}" alt="" />
                                 </div>
                                 <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
                                     <div class="entity-view-popup">
-                                        <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
+                                        <a class="content-link action-icon-bordered rounded-circle" href="{{asset('storage/'.$movie->photo)}}" data-magnific-popup="image">
                                             <span class="icon-content"><i class="fas fa-search"></i></span>
                                         </a>
                                     </div>
-                                    <h4 class="entity-title">The image title</h4>
+                                    <h4 class="entity-title">{{$movie->name}}</h4>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="bg-theme-lighted d-over collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="action-icon-theme action-icon-bordered rounded-circle" href="https://www.youtube.com/watch?v=d96cjJhvlMA" data-magnific-popup="iframe">
-                                            <span class="icon-content"><i class="fas fa-play"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The video title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
-                                            <span class="icon-content"><i class="fas fa-search"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The image title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
-                                            <span class="icon-content"><i class="fas fa-search"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The image title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
-                                            <span class="icon-content"><i class="fas fa-search"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The image title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="bg-theme-lighted d-over collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="action-icon-theme action-icon-bordered rounded-circle" href="https://www.youtube.com/watch?v=d96cjJhvlMA" data-magnific-popup="iframe">
-                                            <span class="icon-content"><i class="fas fa-play"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The video title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
-                                            <span class="icon-content"><i class="fas fa-search"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The image title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="bg-theme-lighted d-over collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="action-icon-theme action-icon-bordered rounded-circle" href="https://www.youtube.com/watch?v=d96cjJhvlMA" data-magnific-popup="iframe">
-                                            <span class="icon-content"><i class="fas fa-play"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The video title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="gallery-entity">
-                            <div class="entity-preview" data-role="hover-wrap">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <img class="embed-responsive-item" src="http://via.placeholder.com/1920x1080" alt="" />
-                                </div>
-                                <div class="d-over bg-black-40 collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
-                                    <div class="entity-view-popup">
-                                        <a class="content-link action-icon-bordered rounded-circle" href="http://via.placeholder.com/1920x1080" data-magnific-popup="image">
-                                            <span class="icon-content"><i class="fas fa-search"></i></span>
-                                        </a>
-                                    </div>
-                                    <h4 class="entity-title">The image title</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
-                <div class="section-bottom">
-                    <div class="paginator">
-                        <a class="paginator-item" href="#"><i class="fas fa-chevron-left"></i></a>
-                        <a class="paginator-item" href="#">1</a>
-                        <span class="active paginator-item">2</span>
-                        <a class="paginator-item" href="#">3</a>
-                        <span class="paginator-item">...</span>
-                        <a class="paginator-item" href="#">10</a>
-                        <a class="paginator-item" href="#"><i class="fas fa-chevron-right"></i></a>
-                    </div>
-                </div>
-            </div>
-        </section>
+            </div> 
+
+</section>
+
+
+
+
+
+
+
+
+        
+
+
         <a class="scroll-top disabled" href="#"><i class="fas fa-angle-up" aria-hidden="true"></i></a>
         
         <!-- jQuery library -->
